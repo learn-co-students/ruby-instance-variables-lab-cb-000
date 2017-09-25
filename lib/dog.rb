97b08a997a38b16c385c3b4c22c560610be7b439
Chris Nunez <cenunez47@gmail.com>
2017-09-25 17:15:08 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,5 @@
 class Dog
+
   def name=(dog_name)
     @this_dogs_name = dog_name
   end
@@ -6,8 +7,5 @@ class Dog
   def name
     @this_dogs_name
   end
-end
 
-lassie = Dog.new
-lassie.name = "Lassie"
-lassie.name
+end
